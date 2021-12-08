b99631a3e10fec85e26e2248c51b8996e79febb4
Martin Wu <mw0922365@otc.edu>
2021-12-08 16:23:40 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk");
+const chalkAnimation = require('chalk-animation');
+const log = console.log
+
+chalkAnimation.rainbow('This is a story about the most unlucky person alive.');
+
+log(chalk.red("Hello World"));
