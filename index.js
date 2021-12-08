ef8a1b98a5cfae1aef1ffd2678c279f5c0c65a74
Martin <35703665+Mwu84@users.noreply.github.com>
2021-12-08 16:40:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const log = console.log
 
 chalkAnimation.rainbow('This is a story about the most unlucky person alive.');
 
-log(chalk.red("Hello World"));
+log(chalk.red("Otto is the most unlucky person alive, He chokes on his own spit, step on banana peel and fall, and etcs.") +
+    chalk.Yellow("Otto is cursed and destined to be unlucky forever") +
+    chalk.green("and he accepted the fact."));
